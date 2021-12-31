6ae409d32bc244cbbb908b4ab4269fb32a1b5278
stephte <teaganstephenson@gmail.com>
2021-12-31 13:55:08 -0500

    updated seed data and package.json name

--- db/seeds.rb
+++ db/seeds.rb
@@ -73,6 +73,14 @@ scorecards = Scorecard.create([
       Player.new(
         name: "Phil",
         scores: [3,4,4,4,4,3,5,3,4, 2,4,3,3,3,4,5,4,4]
+      ),
+      Player.new(
+        name: "DJ",
+        scores: [4,3,4,5,3,4,4,4,3, 4,4,3,3,3,4,4,4,4]
+      ),
+      Player.new(
+        name: "You",
+        scores: [3,3,4,4,3,4,4,3,3, 3,4,3,3,4,3,4,4,4]
       )
     ]
   }
